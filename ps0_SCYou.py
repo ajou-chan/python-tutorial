bb7c74444efacfa6e8ef853c45c70fb08e9f74be
ajou-chan <chandryou@ajou.ac.kr>
2016-09-05 16:44:38 +0900

    Add files via upload

---
+++ ps0_SCYou.py
@@ -0,0 +1,14 @@
+
+# coding: utf-8
+
+# In[3]:
+
+# Problem Set 0
+# Name: SC You
+# Time: about 10 min
+
+last = input("Enter your last name:")
+first = input("Enter your first name:")
+
+print ("Your name is", first + last)
+
