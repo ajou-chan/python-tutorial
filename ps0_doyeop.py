990b21e3151ff3b4f89904496b434adff4fb0745
hidoyebi <hidoyebi@ajou.ac.kr>
2016-09-05 19:03:59 +0900

    도엽 과제 #0
    도엽 과제 #0

---
+++ ps0_doyeop.py
@@ -0,0 +1,12 @@
+# Problem Set 0
+# Name : Doyeop Kim
+# Collaborators: no one\
+# Time: 17:50
+
+# 1. Asks the user to enter his/her last name.
+# 2. Asks the user to enter his/her first name.
+# 3. Print out the user's first and last name in that order.
+
+lastName = input('Enter your last name: \n')
+firstName = input('Enter your first name: \n')
+print(lastName + "\n" + firstName)
